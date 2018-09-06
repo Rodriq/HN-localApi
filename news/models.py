c50b6c7a2e9a3a528a553188343cfc3b4e07eae1
Rodriq <fogangrodriq@gmail.com>
2018-09-06 06:58:11 +0100

    modifying model

--- news/models.py
+++ news/models.py
@@ -6,7 +6,6 @@ class News(models.Model):
 	title = models.CharField(max_length=255, null=False)
 	body = models.TextField(max_length=4000)
 	url = models.TextField(max_length=255)
-	god = models.CharField(max_length=255, default='')
 	created_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
